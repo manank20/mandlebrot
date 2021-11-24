c0df901a981c66d88bd180c631dbc228ba5f1ed8
Manank Patel <f20201696@goa.bits-pilani.ac.in>
2021-11-24 21:33:21 +0530

    add esc_time and parse pair functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use num::Complex;
+use std::str::FromStr;
+
+
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex{re: 0.0, im: 0.0};
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z*z + c;
+    }
+    None
+}
+
+fn parse_pair<T: FromStr> (s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => { match (T::from_str(&s[..index]), T::from_str(&s[index+1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None
+            }
+        }
+    }
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
