3ec3777522cb1d09937b37307408ae0cfec2ffae
Manank Patel <f20201696@goa.bits-pilani.ac.in>
2021-11-24 22:37:48 +0530

    add final functions and main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use num::Complex;
 use std::str::FromStr;
-use test::RunIgnored::No;
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
+use std::env;
+use std::env::args;
 
 
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
@@ -27,18 +31,25 @@ fn parse_pair<T: FromStr> (s: &str, separator: char) -> Option<(T, T)> {
 
 fn parse_complex(s: &str) -> Option<Complex<f64>> {
     match parse_pair(s, ',') {
-        some(re, im) => Some(Complex{re, im}),
+        Some((re, im)) => Some(Complex{re, im}),
         None => None
     }
 }
 
-fn pixel_to_point(bounds: usize, pixel: usize, upper_left: Complex<f64>, lower_right: Complex<f64>) -> Complex<f64>{
+fn pixel_to_point(bounds: (usize, usize), pixel: (usize, usize), upper_left: Complex<f64>, lower_right: Complex<f64>) -> Complex<f64>{
     let (width, height) = (lower_right.re - upper_left.re,  upper_left.im - lower_right.im);
-    Complex {re: upper_left.re + pixel.0 * width as f64 / bounds.0 as f64, im: upper_left.im - pixel.1 * height as f64/ bounds.1 as f64}
+    Complex {re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64, im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64}
+}
+
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error>{
+    let output = File::create(filename)?;
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+    Ok(())
 }
 
 fn render(pixels: &mut [u8], bounds: (usize, usize), upper_left: Complex<f64>, lower_right: Complex<f64>) {
-    assert!(pixels.len(), bounds.0 * bounds.1);
+    assert_eq!(pixels.len(), bounds.0 * bounds.1);
     for row in 0..bounds.1 {
         for column in 0..bounds.0 {
             let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
@@ -51,5 +62,20 @@ fn render(pixels: &mut [u8], bounds: (usize, usize), upper_left: Complex<f64>, l
 }
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} file pixels upperleft lowerright", args[0]);
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error left corner");
+    let lower_rignt = parse_complex(&args[4]).expect("error right corner");
+    let mut pixels = vec![0; bounds.0*bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_rignt);
+
+    write_image(&args[1], &pixels, bounds).expect("error writing file");
+
 }
